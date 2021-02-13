1e6bd886005a44ec55d37eb62338f2d9badb9976
matrs01 <matrs011@gmail.com>
2021-02-13 20:58:49 +0300

    add header Timer.h

---
+++ homework_1/Timer.h
@@ -0,0 +1,38 @@
+#ifndef ADVANCED_CPP_COURSE_4_SEM_TIMER_H
+#define ADVANCED_CPP_COURSE_4_SEM_TIMER_H
+
+
+class Timer{
+public:
+    using clock_time = std::chrono::steady_clock;
+    using time_point_t = clock_time::time_point;
+    using time_duration = std::chrono::nanoseconds;
+    Timer() : m_begin(clock_time::now()) {};
+    ~Timer(){
+        auto end = clock_time::now();
+
+        std::cout<<"nanoseconds: "<<std::chrono::duration_cast<
+                std::chrono::nanoseconds> (time_period + (end-m_begin)*(1-stopped)).count() << std::endl;
+    }
+    void StopTimer() {
+        auto end = clock_time::now();
+        if (!stopped){
+            time_period += end - m_begin;
+            stopped = true;
+        }
+    }
+    void SetTimer() {
+        if (stopped){
+            m_begin = clock_time::now();
+            stopped = false;
+        }
+    }
+
+private:
+    time_point_t m_begin;
+    bool stopped = false;
+    time_duration time_period {0};
+};
+
+
+#endif //ADVANCED_CPP_COURSE_4_SEM_TIMER_H
