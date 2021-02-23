338bb02ce084db6bfc650be8db008528794ad932
matrs01 <matrs011@gmail.com>
2021-02-23 16:21:07 +0300

    homework_3/my_hash_for_double change visualization

--- homework_3/my_hash_for_double/hist_maker.py
+++ homework_3/my_hash_for_double/hist_maker.py
@@ -6,4 +6,6 @@ data = pd.read_csv('statistics/stats.csv', header=None).values[0][:-1]
 # print(data)
 fig, ax =plt.subplots(figsize=(6,4))
 plt.bar([i for i in range(len(data))], data, width=0.6)
+ax.set_ylabel("number_of_elements_in_bucket")
+ax.set_xlabel("bucket_index")
 plt.savefig('statistics/stats.pdf')
