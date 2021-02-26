8a16918b1734d440181583e7bc4376b554406c33
matrs01 <matrs011@gmail.com>
2021-02-26 18:52:10 +0300

    homework_3/hash_function_from_boost_v2 little changes

--- homework_3/hash_function_from_boost_v2/plot_maker.py
+++ homework_3/hash_function_from_boost_v2/plot_maker.py
@@ -6,11 +6,11 @@ x = df['Number_of_instances'].values
 y = df['Number_of_collisions'].values
 fig, ax = plt.subplots(1, 2, figsize=(12, 5))
 ax[0].plot(x, y)
-ax[0].set_xlabel("load_factor")
+ax[0].set_xlabel("num_of_instances")
 ax[0].set_ylabel("number_of_collisions")
 ax[0].grid()
 ax[1].plot(x**2, y)
-ax[1].set_xlabel("load_factor${}^{2}$")
+ax[1].set_xlabel("num_of_instances${}^{2}$")
 ax[1].set_ylabel("number_of_collisions")
 ax[1].grid()
 plt.savefig('statistics/plot.pdf')
