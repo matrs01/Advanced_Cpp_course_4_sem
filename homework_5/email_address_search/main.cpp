34b681286d24daed3937f29c8ffa10de345780c5
matrs01 <matrs011@gmail.com>
2021-03-09 20:19:42 +0300

    homework_5/email_address_search done

---
+++ homework_5/email_address_search/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+int main() {
+    std::regex pattern(R"(([_\-\w]+)@([_\w\-]+)\.([[:alpha:]]+[_\-\.?[:alpha:]+]*))");
+    std::string input_str;
+    std::getline(std::cin, input_str);
+    std::sregex_iterator begin(input_str.cbegin(), input_str.cend(), pattern);
+    std::sregex_iterator end;
+
+    std::for_each(begin, end, [](const std::smatch &m) {
+        std::cout << "e-mail: " << m[0] << ", domain name: " << m[3] << '\n';
+    });
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
