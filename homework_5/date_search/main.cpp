facab29a05a0dd314dbbb72af9488d128e6740e3
matrs01 <matrs011@gmail.com>
2021-03-12 01:20:19 +0300

    homework_5/date_search fixed

--- homework_5/date_search/main.cpp
+++ homework_5/date_search/main.cpp
@@ -5,8 +5,8 @@
 int main() {
 //    date and time format: YYYY.MM.DD and HH:MM:SS
     std::regex date_pattern(
-            R"(([1-9][0-9]{3}|0[1-9][0-9]{2}|00[1-9][0-9]|000[1-9]).(0[0-9]|1[012]).([012][0-9]|3[01]))");
-    std::regex time_pattern(R"(([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]))");
+            R"((\d{4}).(0\d|1[012]).([012]\d|3[01]))");
+    std::regex time_pattern(R"(([01]\d|2[0-3])(:[0-5]\d){2})");
     std::string input_str;
     std::getline(std::cin, input_str);
     std::sregex_iterator date_begin(input_str.cbegin(), input_str.cend(), date_pattern);
@@ -21,7 +21,7 @@ int main() {
     std::for_each(time_begin, end, [](const std::smatch &m) {
         std::cout << m[0] << ' ';
     });
-
+    std::cout << std::endl;
     system("pause");
     return EXIT_SUCCESS;
 }
