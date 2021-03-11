a9454b728aa6022aa59fb59ac7298f0765597916
matrs01 <matrs011@gmail.com>
2021-03-12 01:51:20 +0300

    homework_5/email_address_search fix

--- homework_5/email_address_search/main.cpp
+++ homework_5/email_address_search/main.cpp
@@ -3,7 +3,11 @@
 #include <string>
 
 int main() {
-    std::regex pattern(R"(([\-\w]+)@([\w\-]+\.[[:alpha:]]+[_\-\.?[:alpha:]+]*))");
+//    std::regex pattern(R"(([\-\w]+)@([\w\-]+\.[[:alpha:]]+[_\-.?[:alpha:]+]*))");
+    std::regex pattern(R"((([\-\w]+\.)*[\-\w])+@(([[:alpha:]]+\.)+[[:alpha:]]{2,6}))");
+//    std::regex pattern(R"(([\-\w]+)@(([[:alpha:]]+\.)+))");
+
+
     std::string input_str;
     std::getline(std::cin, input_str);
     std::sregex_iterator begin(input_str.cbegin(), input_str.cend(), pattern);
@@ -11,7 +15,7 @@ int main() {
 
     std::for_each(begin, end, [](const std::smatch &m) {
 
-        std::cout << "e-mail: " << m[0] << ", domain name: " << m[2] << '\n';
+        std::cout << "e-mail: " << m[0] << ", domain name: " << m[3] << '\n';
     });
 
     system("pause");
