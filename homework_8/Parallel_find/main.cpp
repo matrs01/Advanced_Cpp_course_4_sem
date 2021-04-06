1cfbde097e1deb9acfb30f92d9d7c22e04dad77e
matrs01 <matrs011@gmail.com>
2021-04-06 18:37:38 +0300

    homework_8/Parallel_find fix

--- homework_8/Parallel_find/main.cpp
+++ homework_8/Parallel_find/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <mutex>
 
 
 template < typename Iterator >
@@ -20,22 +21,22 @@ struct Searcher
             {
                 return false;
             }
-            std::advance(first, 1);
-            std::advance(second, 1);
+            first++;
+            second++;
         }
         return true;
     }
-    void operator()(Iterator first, Iterator last, std::string & element,
-                    std::vector < int > & container, size_t index)
+    void operator()(Iterator first, Iterator last, const std::string & element,
+                    std::vector < int > & container, size_t index, std::mutex & mutex)
     {
-        const std::size_t size = element.size();
-        for (; (first != last); ++first)
+        for (const std::size_t size = element.size()
+                ; (first != last); ++first, ++index)
         {
-            if (compare(first, element.begin(), size))
+            if (compare(first, element.cbegin(), size))
             {
+                std::scoped_lock lock(mutex);
                 container.push_back(index);
             }
-            index++;
         }
     }
 };
@@ -46,18 +47,12 @@ void parallel_find(Iterator first, Iterator last, std::string & element, std::ve
     const std::size_t length = std::distance(first, last);
     const int size = element.size();
 
+    std::mutex mutex;
+
     if (!length)
         return;
 
-    const std::size_t min_per_thread = 25;
-    const std::size_t max_threads =
-            (length + min_per_thread - 1) / min_per_thread;
-
-    const std::size_t hardware_threads =
-            std::thread::hardware_concurrency();
-
-    const std::size_t num_threads =
-            std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+    const std::size_t num_threads = std::thread::hardware_concurrency();
 
     const std::size_t block_size = length / num_threads;
 
@@ -65,23 +60,25 @@ void parallel_find(Iterator first, Iterator last, std::string & element, std::ve
 
     {
         Iterator block_start = first;
+        Iterator block_end = first;
+
         std::size_t index = 0;
 
         for (std::size_t i = 0; i < (num_threads - 1); ++i)
         {
-            Iterator block_end = block_start;
             std::advance(block_end, block_size + std::min(size, std::distance(block_end, last)) - size + 1);
             index = std::distance(first, block_start);
 
-            threads[i] = std::async(Searcher < Iterator> (),
-                                     block_start, block_end, std::ref(element), std::ref(container), index);
+            threads[i] = std::async(std::launch::async, Searcher < Iterator> (),
+                                     block_start, block_end, std::ref(element), std::ref(container),
+                                     index, std::ref(mutex));
 
             block_start = block_end;
         }
         index = std::distance(first, block_start);
         if (std::distance(block_start, last) >= size)
         {
-            Searcher < Iterator> ()(block_start, last, element, container, index);
+            Searcher < Iterator> ()(block_start, last, element, container, index, mutex);
         }
     }
 }
@@ -97,7 +94,7 @@ int main(int argc, char ** argv)
     std::getline(std::cin, line);
 
     std::vector < int > indexes;
-    parallel_find(data.begin(), data.end(), line, indexes);
+    parallel_find(data.cbegin(), data.cend(), line, indexes);
     std::sort(indexes.begin(), indexes.end());
 
     if (indexes.empty())
