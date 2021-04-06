26f0c2c7b578bb7801adafb7068a5fc1435909bc
matrs01 <matrs011@gmail.com>
2021-04-06 15:22:59 +0300

    homework_8/Pi_Monte-Carlo_parallel_v2 fix

--- homework_8/Pi_Monte-Carlo_parallel_v2/main.cpp
+++ homework_8/Pi_Monte-Carlo_parallel_v2/main.cpp
@@ -5,9 +5,9 @@
 #include <cstdint>
 #include "../../homework_1/Timer.h"
 
-std::atomic < int > counter = 0;
 
-void sequential_algorithm (std::uint64_t N) {
+
+void sequential_algorithm (std::uint64_t N, std::atomic < int > & counter) {
     std::uniform_real_distribution < double > distribution(0.0, 2.0);
     std::random_device rd;  // так как треды создаются неодновременно, эта величина у них будет разная
     std::mt19937 engine(rd());
@@ -25,18 +25,18 @@ void sequential_algorithm (std::uint64_t N) {
     }
 }
 
-auto parallel_algorithm (std::uint64_t N) {
+auto parallel_algorithm (std::uint64_t N, std::atomic < int > & counter) {
     const std::uint64_t num_threads = std::thread::hardware_concurrency();
     const std::uint64_t block_size = N / num_threads;
     std::vector < std::future < void > > futures(num_threads - 1);
 
     for (auto i = 0U; i < num_threads-1; i++)
     {
-        futures[i] = std::async(std::launch::async, sequential_algorithm, block_size);
+        futures[i] = std::async(std::launch::async, sequential_algorithm, block_size, std::ref(counter));
     }
 
     auto last_N = N - block_size*(num_threads-1);
-    sequential_algorithm(last_N);
+    sequential_algorithm(last_N, std::ref(counter));
 
     for (auto i = 0U; i < num_threads-1; i++)
     {
@@ -47,9 +47,10 @@ auto parallel_algorithm (std::uint64_t N) {
 }
 
 int main() {
+    std::atomic < int > counter = 0;
     std::cout << "Parallel algorithm working:\n";
     Timer time;
-    auto pi = parallel_algorithm(100000000);
+    auto pi = parallel_algorithm(100000000, counter);
     time.StopTimer();
     std::cout << pi << std::endl;
     auto parallel_algorithm_time = time.GetTime();
